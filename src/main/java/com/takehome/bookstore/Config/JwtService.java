5136d0fd04072e9a9aafcb3c328050ae399395f1
iz.aia.h <imukisa024@gmail.com>
2024-04-14 17:25:12 +0300

    Authentication works

--- src/main/java/com/takehome/bookstore/Config/JwtService.java
+++ src/main/java/com/takehome/bookstore/Config/JwtService.java
@@ -18,7 +18,7 @@ import io.jsonwebtoken.security.Keys;
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY = "QewCJsxBRaVbvDhUP1JRTedVbX6hqpC1";
+    private static final String SECRET_KEY = "OK9uRHuEgNxtTn9GxW0cc145DKVDan8N3JkCA92h75biOTyihvT22LEnHKOnjurhTKzgzegR92ReU+cjqskIOVwHwrXfXQ06Fh2vPaZXJNFpsS96qE7V6GiqA8GsCyIgEaPIKedNyISMb+CDToCzFHsW7z4egnbPsF5uKGA012Wed0hP2DqPTA1hG91F9bPxC9dor3+rQ5XaTSQUGi5GoNx5dxyzICuyRsAT+Tm5vJhWIpLcwpeifSGvPlVgCDjjdf2ZWecbSpEGJTja73U5WRSqklaNl6h8sFchCfJc56uwYaKaMQ+XaBHN1wEHF2n3dVk9FBTfaZBd64/fd0Fttw8le8FdcFgWUY/2v7VkgU0";
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);

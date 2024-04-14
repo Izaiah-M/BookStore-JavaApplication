412715ece568bae6d7f995b514d5e9f914954c64
iz.aia.h <imukisa024@gmail.com>
2024-04-14 22:07:53 +0300

    finished Swagger config

--- src/main/java/com/takehome/bookstore/users/UsersController.java
+++ src/main/java/com/takehome/bookstore/users/UsersController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
 @RestController
@@ -13,6 +14,7 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 public class UsersController {
 
     @GetMapping
+    @Hidden
     public ResponseEntity<String> sayHello() {
         return ResponseEntity.ok("Hello, user! this is a secure endpoint though");
     }

a65ef3a7f85a21221c4606bdba95205ebe40c50f
iz.aia.h <imukisa024@gmail.com>
2024-04-14 22:04:36 +0300

    Added more specific things to it

--- src/main/java/com/takehome/bookstore/auth/AuthenticationController.java
+++ src/main/java/com/takehome/bookstore/auth/AuthenticationController.java
@@ -6,16 +6,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
+@Tag(name = "Authentication")
 public class AuthenticationController {
 
     private final AuthenticationService service;
 
     @PostMapping("/register")
+    @Operation(description = "POST Registration Endpoint", summary = "This is for registering a user, the default role is USER")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
 
         return ResponseEntity.ok(service.register(request));

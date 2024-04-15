f218fe3a0047c90b433b79d3980ba3425f383120
iz.aia.h <imukisa024@gmail.com>
2024-04-15 12:34:38 +0300

    stopped tracking application.properties

--- src/main/java/com/takehome/bookstore/Config/JwtService.java
+++ src/main/java/com/takehome/bookstore/Config/JwtService.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +19,8 @@ import io.jsonwebtoken.security.Keys;
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY = "OK9uRHuEgNxtTn9GxW0cc145DKVDan8N3JkCA92h75biOTyihvT22LEnHKOnjurhTKzgzegR92ReU+cjqskIOVwHwrXfXQ06Fh2vPaZXJNFpsS96qE7V6GiqA8GsCyIgEaPIKedNyISMb+CDToCzFHsW7z4egnbPsF5uKGA012Wed0hP2DqPTA1hG91F9bPxC9dor3+rQ5XaTSQUGi5GoNx5dxyzICuyRsAT+Tm5vJhWIpLcwpeifSGvPlVgCDjjdf2ZWecbSpEGJTja73U5WRSqklaNl6h8sFchCfJc56uwYaKaMQ+XaBHN1wEHF2n3dVk9FBTfaZBd64/fd0Fttw8le8FdcFgWUY/2v7VkgU0";
+    @Value("${secret.key}")
+    private String SECRET_KEY;
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
